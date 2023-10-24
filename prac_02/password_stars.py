7b6d2db4121843e0ea686e32bf718bb3e3709aa6
jiahuan <liujh22@163.com>
2023-10-23 22:59:25 -0400

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,12 +1,21 @@
 
-password = input("Enter your password: ")
+def main():
+    password = get_password()
+    print_asterisks(password)
 
-while len(password) < 10:
-    print("Too Short")
+
+def get_password():
     password = input("Enter your password: ")
+    while len(password) < 10:
+        print("Too Short")
+        password = input("Enter your password: ")
+    return password
 
-password_length = len(password)
 
-for i in range(password_length):
-    print("*", end="")
+def print_asterisks(the_password):
+    password_length = len(the_password)
+    for i in range(password_length):
+        print("*", end="")
 
+
+main()
