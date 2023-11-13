85b79f25459bcacede0e88fe4bbf25b0497687c8
jiahuan <liujh22@163.com>
2023-11-13 15:13:46 -0500

    询问Email并自动生成name list

---
+++ prac_05/email.py
@@ -0,0 +1,23 @@
+"""
+Practice_05, get email from user and store in dictionary
+return email and their names
+"""
+
+email_to_name = {}
+email = input("Email: ")
+while email != "":
+    """Ask for email, generate or request name, store in dictionary"""
+    name = email.split("@")[0].title().split(".")
+    name = " ".join(name)
+    email_to_name[email] = name
+    confirm = input(f"Is your name {name}? (Y/n) ")
+    if confirm.upper() == "Y" or confirm.upper() == "":
+        pass
+    else:
+        email_to_name[email] = input("Name: ")
+    email = input("Email: ")
+
+print()  # Blank line
+for i in email_to_name:
+    """Print all items in dictionary"""
+    print(f"{email_to_name[i]} ({i})")
