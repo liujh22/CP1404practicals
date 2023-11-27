f55050c7f9dc779344e9734700234e2c2c83c50f
jiahuan <liujh22@163.com>
2023-11-26 20:37:53 -0500

    client code for taxi

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,14 @@
+"""
+Client code to test Taxi.py
+"""
+
+from prac_09.taxi import Taxi
+
+my_taxi = Taxi("Prius 1", 100, 1.23)  # Create new taxi
+my_taxi.drive(40)  # Drive 40km
+print(my_taxi)  # Print taxi detail
+print(f"Your price is {my_taxi.get_fare()}")  # Print current fare
+my_taxi.start_fare()  # Start new fare
+my_taxi.drive(100)  # Drive 100km
+print(my_taxi)  # Print taxi detail
+print(f"Your price is {my_taxi.get_fare()}")  # Print current fare
