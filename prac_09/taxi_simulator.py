25039eed32855b8884ea1a7472ba6afc4a7140a6
jiahuan <liujh22@163.com>
2023-11-28 12:39:29 -0500

    update try... except...

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,7 +1,7 @@
 """
 Client code of Taxi and SilverServiceTaxi
 """
-
+from prac_09.car import Car
 from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
@@ -19,10 +19,10 @@ def main():
             print("Taxis available:")
             print_taxi_list(taxi_list)
             taxi_number = int(input("Choose taxi: "))  # choose a taxi
-            if taxi_number >= len(taxi_list):  # check availability
-                print("Invalid taxi choice")
-            else:
+            try:
                 choose_taxi = taxi_list[taxi_number]
+            except IndexError:  # Error checking
+                print("Invalid taxi choice")
 
         elif choice == "d":  # "d" to drive taxi
             if choose_taxi == "":  # check availability
